e969d3a0ecb9dbfac8d5a5871f5e094486e128e7
delorency <delorency>
2023-11-21 15:39:28 +0300

    reconstructe starter functionality

--- app/main.py
+++ app/main.py
@@ -1,10 +1,31 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.core.container import Container
+from app.core.config import configs
 
 
 
 class ContainerIni:
 
 	def __init__(self):
+
+		# App
+		self.app = FastAPI(
+			title=configs.PROJECT_NAME,
+			openapi_url=f"{configs.API}/openapi.json",
+			version="0.0.1",
+		)
+
+		# Middleware
+		if configs.BACKEND_CORS_ORIGINS:
+			self.app.add_middleware(
+				CORSMiddleware,
+				allow_origins=[str(origin) for origin in configs.BACKEND_CORS_ORIGINS],
+				allow_credentials=True,
+				allow_methods=["*"],
+				allow_headers=["*"],
+			)
 		
 		# Container
 		self.container = Container()
@@ -15,10 +36,13 @@ class ContainerIni:
 		# Redis
 		self.redis = self.container.redis()
 
+		# Add routes
+
 
 
 container_ini = ContainerIni()
 
+app = container_ini.app
 database = container_ini.database
 redis = container_ini.redis
 container = container_ini.container
--- start.py
+++ start.py
@@ -1,41 +1,5 @@
 import uvicorn
 
-from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
-
-from app.core.config import configs
-
-from app.core.container import Container
-from app.main import container_ini
-
-
-
-class AppCreator:
-	def __init__(self):
-		# App
-		self.app = FastAPI(
-			title=configs.PROJECT_NAME,
-			openapi_url=f"{configs.API}/openapi.json",
-			version="0.0.1",
-		)
-
-		# Middleware
-		if configs.BACKEND_CORS_ORIGINS:
-			self.app.add_middleware(
-				CORSMiddleware,
-				allow_origins=[str(origin) for origin in configs.BACKEND_CORS_ORIGINS],
-				allow_credentials=True,
-				allow_methods=["*"],
-				allow_headers=["*"],
-			)
-
-		# Add routes
-
-
-
-app_creator = AppCreator()
-app = app_creator.app
-
 
 if __name__ == '__main__':
-	uvicorn.run("start:app", host="0.0.0.0", log_level="info")
+	uvicorn.run("app.main:app", host="0.0.0.0", log_level="info")
